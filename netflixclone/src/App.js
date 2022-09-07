9c12e9f3aeef8c83f57150849eb5fd190aa1cf40
rclepf <rclepf@gmail.com>
2022-09-07 13:47:18 -0300

    conexao api

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import MovieRow from './components/MovieRow';
+import tmdb from './tmdb';
+
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div>
-      Olá!
+    <div className='page'>
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
---
+++ netflixclone/src/components/MovieRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MovieRow.css';
+
+export default ({ title, items }) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+        </div>
+    );
+}
+
+
